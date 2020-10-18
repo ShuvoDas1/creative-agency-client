f3b91264d68e5235392c6740a880ea44ec0d8ccf
ShuvoDas1 <67542645+ShuvoDas1@users.noreply.github.com>
2020-10-18 11:31:55 +0600

    pushed

--- src/components/Home/HeaderMain/HeaderMain.js
+++ src/components/Home/HeaderMain/HeaderMain.js
@@ -7,7 +7,7 @@ const HeaderMain = () => {
     return (
         <main className='row d-flex align-item-center' style={{marginTop:'20px'}}>
             <div className="col-md-4 col-12 ml-5 ">
-                <h1 className='text-black'> Let's Grew Your  Brand To The NextLavel </h1>
+                <h2 className='text-black'> Let's Grew Your  Brand To The NextLavel </h2>
                 <br/>
                 <p className='text-black'><small>Lorem ipsum dolor sit amet consectetur, <br/> adipisicing elit. Culpa aliquid in, <br/> quisquam voluptates quam adipisci?</small></p>
                 <br/>
